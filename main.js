4eb9e702f6cf800bda38fdf225c4a572473ce303
karimriveraapolinar <kariveraapolinar@my.waketech.edu>
2025-05-07 11:49:36 -0400

    commented

--- main.js
+++ main.js
@@ -18,7 +18,10 @@ function setupElement(startAt, amount, parent) {
                        (i >= 75 && i <= 89) ? i + 29 :
                        (i >= 90 && i <= 103) ? i - 32 :
                        (i >= 104 && i <= 117) ? i - 14 : i + 1);
-
+        // an event listener is added to the button which will trigger
+        // when the button is clicked The function elementClicked
+        // will be called and the page will be redirected to the
+        // corresponding element page
         button.addEventListener("click", function elementClicked() {
             window.location.href = `https://marcusceradini.github.io/Project/elements/element${trueI}.html`
         });
@@ -46,20 +49,26 @@ function setupTable() {
 
 // The function is called and the table is setup
 setupTable();
-
+//A search bar is created and defined as input 
 const searchContainer = document.createElement("div");
 const searchInput = document.createElement("input");
+// The input type is set to number to restrict input to numbers
 searchInput.type = "number";
+// The possible input values are restricted to 1-118 but not enforced using javascript
 searchInput.placeholder = "Enter element number (1–118)";
 searchInput.style.margin = "10px";
-
+// The button is created
 const searchButton = document.createElement("button");
+// The button is given a text value
 searchButton.textContent = "Search";
-
+//the button and search bar are given a couple words detailing what needs to be input
 searchContainer.appendChild(searchInput);
 searchContainer.appendChild(searchButton);
+// the searchcontainer is placed before the periodic table
 document.body.insertBefore(searchContainer, periodicTable);
 
+
+// The button is given an event listener which will trigger when the button is clicked
 searchButton.addEventListener("click", () => {
     const atomicNumber = parseInt(searchInput.value);
     if (isNaN(atomicNumber) || atomicNumber < 1 || atomicNumber > 118) {
@@ -75,18 +84,20 @@ searchButton.addEventListener("click", () => {
                       (i >= 75 && i <= 89) ? i + 29 :
                       (i >= 90 && i <= 103) ? i - 32 :
                       (i >= 104 && i <= 117) ? i - 14 : i + 1);
+        // if the element is found, the id is stored and loop is broken
         if (trueI === atomicNumber) {
             foundId = `element${i}`;
             break;
         }
     }
-
+    // If the element is found, it will be scrolled to and highlighted
     if (foundId) {
         const button = document.getElementById(foundId);
         button.scrollIntoView({ behavior: "smooth", block: "center" });
         button.style.backgroundColor = "yellow";
         setTimeout(() => button.style.backgroundColor = "", 1500);
     } else {
+        // If the element is not found, an alert will be shown
         alert("Element not found.");
     }
 });
